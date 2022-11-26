3ccb8232eb36f3f736367d5b46d4c3c17ffce2f5
Yuki Kishimoto <yukikishimoto@protonmail.com>
2022-11-26 18:04:32 +0100

    Add serde

--- src/util/mod.rs
+++ src/util/mod.rs
@@ -1,8 +1,32 @@
 // Copyright (c) 2022 Yuki Kishimoto
 // Distributed under the MIT software license
 
+use anyhow::{anyhow, Result};
+use serde::de::DeserializeOwned;
+use serde::Serialize;
+
 pub mod aes;
 pub mod convert;
 pub mod dir;
 pub mod hash;
 pub mod io;
+
+pub fn serialize<T>(data: T) -> Result<Vec<u8>>
+where
+    T: Serialize,
+{
+    match serde_json::to_string(&data) {
+        Ok(data) => Ok(data.into_bytes()),
+        Err(_) => Err(anyhow!("Failed to serialize data")),
+    }
+}
+
+pub fn deserialize<T>(data: Vec<u8>) -> Result<T>
+where
+    T: DeserializeOwned,
+{
+    match serde_json::from_slice::<T>(&data) {
+        Ok(data) => Ok(data),
+        Err(_) => Err(anyhow!("Failed to deserialize data")),
+    }
+}
