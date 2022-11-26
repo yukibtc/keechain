80c2ff538c941d8ae2586698ec9e175d57e3fe83
Yuki Kishimoto <yukikishimoto@protonmail.com>
2022-11-26 19:26:30 +0100

    Update keychain files directory

--- src/util/dir.rs
+++ src/util/dir.rs
@@ -9,14 +9,11 @@ pub fn get_directory() -> Result<PathBuf> {
     Ok(match dirs::home_dir() {
         Some(path) => {
             let path: PathBuf = path.join(".keechain");
-
-            // Create directory if not exist
             if !path.exists() {
                 std::fs::create_dir_all(path.clone())?;
             }
-
             path
         }
-        None => Path::new("./").to_path_buf(),
+        None => Path::new("./keechain").to_path_buf(),
     })
 }
