eb76695b0aafe9ef301197adf34c69249ec71141
Yuki Kishimoto <yukikishimoto@protonmail.com>
2023-01-09 10:38:14 +0100

    cli: add vanity command

--- keechain-cli/src/cli/mod.rs
+++ keechain-cli/src/cli/mod.rs
@@ -102,6 +102,14 @@ pub enum AdvancedCommand {
         #[arg(required = true)]
         index: Index,
     },
+    Vanity {
+        /// Keychain name
+        #[arg(required = true)]
+        name: String,
+        /// Prefixes
+        #[arg(required = true)]
+        prefixes: Vec<String>,
+    },
     /// Danger
     Danger {
         #[command(subcommand)]
