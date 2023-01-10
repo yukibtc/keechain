b93d2bf3460af7d0a7912f5c28eace7f5d382225
Yuki Kishimoto <yukikishimoto@protonmail.com>
2023-01-10 13:35:47 +0100

    gui: update theme colors module

--- keechain/src/theme/mod.rs
+++ keechain/src/theme/mod.rs
@@ -2,3 +2,16 @@
 // Distributed under the MIT software license
 
 pub mod color;
+pub mod font;
+
+/* #[derive(Debug, Clone, Copy, PartialEq, Eq)]
+pub enum Theme {
+    Light,
+    Dark,
+}
+
+impl Default for Theme {
+    fn default() -> Theme {
+        Theme::Dark
+    }
+} */
