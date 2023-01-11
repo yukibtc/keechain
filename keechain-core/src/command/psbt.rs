b7bc7720c1f36cd46d8e8793e7799abe54455812
Yuki Kishimoto <yukikishimoto@protonmail.com>
2023-01-11 10:52:14 +0100

    core: psbt module

--- keechain-core/src/command/psbt.rs
+++ keechain-core/src/command/psbt.rs
@@ -1,9 +1,9 @@
-// Copyright (c) 2022 Yuki Kishimoto
+// Copyright (c) 2022-2023 Yuki Kishimoto
 // Distributed under the MIT software license
 
 use std::fs::File;
 use std::io::Read;
-use std::path::PathBuf;
+use std::path::{Path, PathBuf};
 
 use bitcoin::Network;
 
@@ -11,7 +11,11 @@ use crate::error::{Error, Result};
 use crate::types::{Psbt, Seed};
 use crate::util::dir;
 
-pub fn decode_file(psbt_file: PathBuf, network: Network) -> Result<Psbt> {
+pub fn decode_file<P>(path: P, network: Network) -> Result<Psbt>
+where
+    P: AsRef<Path>,
+{
+    let psbt_file = path.as_ref();
     if !psbt_file.exists() && !psbt_file.is_file() {
         return Err(Error::Generic("PSBT file not found.".to_string()));
     }
@@ -21,11 +25,15 @@ pub fn decode_file(psbt_file: PathBuf, network: Network) -> Result<Psbt> {
     Psbt::decode(base64::encode(content), network)
 }
 
-pub fn sign_file_from_seed(seed: &Seed, network: Network, psbt_file: PathBuf) -> Result<bool> {
-    let mut psbt: Psbt = decode_file(psbt_file.clone(), network)?;
+pub fn sign_file_from_seed<P>(seed: &Seed, network: Network, path: P) -> Result<bool>
+where
+    P: AsRef<Path>,
+{
+    let psbt_file = path.as_ref();
+    let mut psbt: Psbt = decode_file(psbt_file, network)?;
     let finalized: bool = psbt.sign(seed)?;
     if finalized {
-        let mut psbt_file = psbt_file;
+        let mut psbt_file: PathBuf = psbt_file.to_path_buf();
         dir::rename_psbt_to_signed(&mut psbt_file)?;
         psbt.save_to_file(psbt_file)?;
     }
