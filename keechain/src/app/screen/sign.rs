b7337ee7256a85a370307e9ea245f99b0b6661fa
Yuki Kishimoto <yukikishimoto@protonmail.com>
2023-01-12 11:37:40 +0100

    gui: add option to save signed PSBT or export with QR code

--- keechain/src/app/screen/sign.rs
+++ keechain/src/app/screen/sign.rs
@@ -3,10 +3,11 @@
 
 use std::path::PathBuf;
 
-use iced::widget::Column;
+use iced::widget::qr_code::{self, QRCode};
+use iced::widget::{Column, Row};
 use iced::{Command, Element, Length};
-use keechain_core::command;
 use keechain_core::types::Psbt;
+use keechain_core::util::dir;
 use rfd::FileDialog;
 
 use crate::app::component::Dashboard;
@@ -20,6 +21,8 @@ pub enum SignMessage {
     SelectPsbtFile,
     Sign,
     Clear,
+    ExportWithQrCode,
+    SaveToFile,
 }
 
 #[derive(Debug, Clone)]
@@ -32,6 +35,7 @@ pub struct PsbtFile {
 pub struct SignState {
     psbt_file: Option<PsbtFile>,
     finish: bool,
+    qr_code: Option<qr_code::State>,
     error: Option<String>,
 }
 
@@ -43,6 +47,7 @@ impl SignState {
     pub fn clear(&mut self) {
         self.psbt_file = None;
         self.finish = false;
+        self.qr_code = None;
         self.error = None;
     }
 }
@@ -58,7 +63,7 @@ impl State for SignState {
                 SignMessage::SelectPsbtFile => {
                     if let Some(path) = FileDialog::new().add_filter("psbt", &["psbt"]).pick_file()
                     {
-                        match command::psbt::decode_file(path.clone(), ctx.network) {
+                        match Psbt::from_file(path.clone(), ctx.network) {
                             Ok(psbt) => {
                                 self.error = None;
                                 self.psbt_file = Some(PsbtFile { psbt, path });
@@ -67,22 +72,45 @@ impl State for SignState {
                         }
                     }
                 }
-                SignMessage::Sign => match command::psbt::sign_file_from_seed(
-                    &ctx.keechain.keychain.seed(),
-                    ctx.network,
-                    self.psbt_file.clone().unwrap().path,
-                ) {
-                    Ok(finalized) => {
-                        if finalized {
-                            self.clear();
-                            self.finish = true;
-                        } else {
-                            self.error = Some("PSBT signing not finalized".to_string());
+                SignMessage::Sign => {
+                    if let Some(psbt_file) = self.psbt_file.as_mut() {
+                        match psbt_file.psbt.sign(&ctx.keechain.keychain.seed) {
+                            Ok(finalized) => {
+                                if finalized {
+                                    self.error = None;
+                                    self.finish = true;
+                                } else {
+                                    self.error = Some("PSBT signing not finalized".to_string());
+                                }
+                            }
+                            Err(e) => self.error = Some(e.to_string()),
                         }
+                    } else {
+                        self.error = Some("PSBT not loaded".to_string());
                     }
-                    Err(e) => self.error = Some(e.to_string()),
-                },
+                }
                 SignMessage::Clear => self.clear(),
+                SignMessage::SaveToFile => {
+                    if let Some(psbt_file) = self.psbt_file.as_mut() {
+                        let mut path: PathBuf = psbt_file.path.clone();
+                        match dir::rename_psbt_to_signed(&mut path) {
+                            Ok(_) => match psbt_file.psbt.save_to_file(path) {
+                                Ok(_) => self.clear(),
+                                Err(e) => self.error = Some(e.to_string()),
+                            },
+                            Err(e) => self.error = Some(e.to_string()),
+                        }
+                    } else {
+                        self.error = Some("PSBT not loaded".to_string());
+                    }
+                }
+                SignMessage::ExportWithQrCode => {
+                    if let Some(psbt_file) = self.psbt_file.as_ref() {
+                        self.qr_code = qr_code::State::new(psbt_file.psbt.as_base64()).ok();
+                    } else {
+                        self.error = Some("PSBT not loaded".to_string());
+                    }
+                }
             }
         }
 
@@ -93,28 +121,45 @@ impl State for SignState {
         let mut content = Column::new();
 
         if self.finish {
-            let icon = Icon::new(&CHECK_CIRCLE)
-                .width(Length::Units(100))
-                .size(100)
-                .style(DARK_GREEN);
-            let sign_again_button =
-                button::primary("Sign again").on_press(Message::Sign(SignMessage::Clear));
+            if let Some(qr_code) = self.qr_code.as_ref() {
+                let clearn_button =
+                    button::primary("Clear").on_press(Message::Sign(SignMessage::Clear));
 
-            content = content.push(icon).push(sign_again_button).spacing(20);
+                content = content
+                    .push(QRCode::new(qr_code).cell_size(2))
+                    .push(clearn_button)
+                    .spacing(20);
+            } else {
+                let icon = Icon::new(&CHECK_CIRCLE)
+                    .width(Length::Units(100))
+                    .size(100)
+                    .style(DARK_GREEN);
+
+                let save_to_file = button::primary("Save to file")
+                    .on_press(Message::Sign(SignMessage::SaveToFile));
+
+                let show_qr_code = button::primary("Show QR Code")
+                    .on_press(Message::Sign(SignMessage::ExportWithQrCode));
+
+                content = content
+                    .push(icon)
+                    .push(Row::new().push(save_to_file).push(show_qr_code).spacing(10))
+                    .spacing(20);
+            }
         } else if self.psbt_file.is_some() {
             let sign_button = button::primary("Sign").on_press(Message::Sign(SignMessage::Sign));
             let clear_button = button::border("Clear").on_press(Message::Sign(SignMessage::Clear));
 
             content = content.push(sign_button).push(clear_button).spacing(10);
-
-            if let Some(error) = &self.error {
-                content = content.push(Text::new(error).color(DARK_RED).view());
-            }
         } else {
             let select_button = button::primary("Select PSBT file")
                 .on_press(Message::Sign(SignMessage::SelectPsbtFile));
 
             content = content.push(select_button);
+        }
+
+        if let Some(error) = &self.error {
+            content = content.push(Text::new(error).color(DARK_RED).view());
         }
 
         Dashboard::new().view(ctx, content)
