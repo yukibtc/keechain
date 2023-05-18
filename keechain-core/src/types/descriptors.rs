f49d6729c3dee363fd32469c30376b5c887d206e
Yuki Kishimoto <yukikishimoto@protonmail.com>
2023-05-18 16:55:03 +0200

    core: add `to_typed_descriptor` to `ToDescriptor` trait

--- keechain-core/src/types/descriptors.rs
+++ keechain-core/src/types/descriptors.rs
@@ -4,7 +4,7 @@
 use std::collections::HashMap;
 use std::str::FromStr;
 
-use bdk::miniscript::descriptor::{Descriptor, DescriptorPublicKey};
+use bdk::miniscript::descriptor::{Descriptor, DescriptorKeyParseError, DescriptorPublicKey};
 use bitcoin::Network;
 
 use super::{Purpose, Seed};
@@ -19,6 +19,8 @@ pub enum Error {
     BIP32(#[from] bitcoin::util::bip32::Error),
     #[error(transparent)]
     Miniscript(#[from] bdk::miniscript::Error),
+    #[error(transparent)]
+    DescriptorKeyParse(#[from] DescriptorKeyParseError),
     #[error("Unsupported derivation path")]
     UnsupportedDerivationPath,
     #[error("Invalid derivation path: purpose not provided")]
@@ -72,11 +74,11 @@ impl Descriptors {
 
             descriptors.external.insert(
                 purpose,
-                descriptor(root_fingerprint, derived_public_key, &path, false)?,
+                typed_descriptor(root_fingerprint, derived_public_key, &path, false)?,
             );
             descriptors.internal.insert(
                 purpose,
-                descriptor(root_fingerprint, derived_public_key, &path, true)?,
+                typed_descriptor(root_fingerprint, derived_public_key, &path, true)?,
             );
         }
 
@@ -120,6 +122,23 @@ where
         account: Option<u32>,
         change: bool,
         network: Network,
+    ) -> Result<DescriptorPublicKey, Error> {
+        let root: ExtendedPrivKey = self.to_bip32_root_key(network)?;
+        let root_fingerprint = root.fingerprint(&SECP256K1);
+        let path = bip32::account_extended_path(purpose.as_u32(), network, account)?;
+        let derived_private_key: ExtendedPrivKey = root.derive_priv(&SECP256K1, &path)?;
+        let derived_public_key: ExtendedPubKey =
+            ExtendedPubKey::from_priv(&SECP256K1, &derived_private_key);
+        let (_, desc) = descriptor(root_fingerprint, derived_public_key, &path, change)?;
+        Ok(desc)
+    }
+
+    fn to_typed_descriptor(
+        &self,
+        purpose: Purpose,
+        account: Option<u32>,
+        change: bool,
+        network: Network,
     ) -> Result<Descriptor<DescriptorPublicKey>, Error> {
         let root: ExtendedPrivKey = self.to_bip32_root_key(network)?;
         let root_fingerprint = root.fingerprint(&SECP256K1);
@@ -127,7 +146,7 @@ where
         let derived_private_key: ExtendedPrivKey = root.derive_priv(&SECP256K1, &path)?;
         let derived_public_key: ExtendedPubKey =
             ExtendedPubKey::from_priv(&SECP256K1, &derived_private_key);
-        descriptor(root_fingerprint, derived_public_key, &path, change)
+        typed_descriptor(root_fingerprint, derived_public_key, &path, change)
     }
 }
 
@@ -136,7 +155,7 @@ fn descriptor(
     pubkey: ExtendedPubKey,
     path: &DerivationPath,
     change: bool,
-) -> Result<Descriptor<DescriptorPublicKey>, Error> {
+) -> Result<(ChildNumber, DescriptorPublicKey), Error> {
     let mut iter_path = path.into_iter();
 
     let purpose: &ChildNumber = match iter_path.next() {
@@ -165,15 +184,23 @@ fn descriptor(
         i32::from(change)
     );
 
-    let descriptor: String = match purpose {
-        ChildNumber::Hardened { index: 44 } => format!("pkh({desc})"),
-        ChildNumber::Hardened { index: 49 } => format!("sh(wpkh({desc}))"),
-        ChildNumber::Hardened { index: 84 } => format!("wpkh({desc})"),
-        ChildNumber::Hardened { index: 86 } => format!("tr({desc})"),
-        _ => return Err(Error::UnsupportedDerivationPath),
-    };
+    Ok((*purpose, DescriptorPublicKey::from_str(&desc)?))
+}
 
-    Ok(Descriptor::from_str(&descriptor)?)
+fn typed_descriptor(
+    root_fingerprint: Fingerprint,
+    pubkey: ExtendedPubKey,
+    path: &DerivationPath,
+    change: bool,
+) -> Result<Descriptor<DescriptorPublicKey>, Error> {
+    let (purpose, desc) = descriptor(root_fingerprint, pubkey, path, change)?;
+    match purpose {
+        ChildNumber::Hardened { index: 44 } => Ok(Descriptor::new_pkh(desc)),
+        ChildNumber::Hardened { index: 49 } => Ok(Descriptor::new_sh_wpkh(desc)?),
+        ChildNumber::Hardened { index: 84 } => Ok(Descriptor::new_wpkh(desc)?),
+        ChildNumber::Hardened { index: 86 } => Ok(Descriptor::new_tr(desc, None)?),
+        _ => Err(Error::UnsupportedDerivationPath),
+    }
 }
 
 #[cfg(test)]
@@ -188,14 +215,32 @@ mod test {
         let seed = Seed::from_mnemonic(mnemonic);
 
         // Tr
-        let desc: Descriptor<DescriptorPublicKey> = seed
+        let desc: DescriptorPublicKey = seed
             .to_descriptor(Purpose::TR, None, false, Network::Bitcoin)
+            .unwrap();
+        assert_eq!(desc.to_string(), String::from("[91ef223d/86'/0'/0']xpub6CjhhJyrYK83TKQq797CMiNzc4bpoJiYRBeb7iQ99T6dXrEgvg24hDw3ZKDJLNMyiy9Sbwqaw8TtCdaE4xXhnYwy7ptpNVfEAKUCcz8PMtP/0/*"));
+
+        // Wpkh
+        let desc: DescriptorPublicKey = seed
+            .to_descriptor(Purpose::WPKH, Some(2345), true, Network::Testnet)
+            .unwrap();
+        assert_eq!(desc.to_string(), String::from("[91ef223d/84'/1'/2345']tpubDCgYuiX1p1eecECkhNc2bLSktmSDoMTj5J3v184ErUXqHTywQ7X5afv51UGfDVSaYzDWvdHhVyJ6UK8fM27EwGByWdczEERfAA9j2nzHUAj/1/*"));
+    }
+
+    #[test]
+    fn test_seed_to_typed_descriptor() {
+        let mnemonic = Mnemonic::from_str("range special tuna oblige own drama trend render harsh army outdoor bulb brisk sing analyst own fork senior stove flash fire bulk umbrella vast").unwrap();
+        let seed = Seed::from_mnemonic(mnemonic);
+
+        // Tr
+        let desc: Descriptor<DescriptorPublicKey> = seed
+            .to_typed_descriptor(Purpose::TR, None, false, Network::Bitcoin)
             .unwrap();
         assert_eq!(desc.to_string(), String::from("tr([91ef223d/86'/0'/0']xpub6CjhhJyrYK83TKQq797CMiNzc4bpoJiYRBeb7iQ99T6dXrEgvg24hDw3ZKDJLNMyiy9Sbwqaw8TtCdaE4xXhnYwy7ptpNVfEAKUCcz8PMtP/0/*)#qkangwzf"));
 
         // Wpkh
         let desc: Descriptor<DescriptorPublicKey> = seed
-            .to_descriptor(Purpose::WPKH, Some(2345), true, Network::Testnet)
+            .to_typed_descriptor(Purpose::WPKH, Some(2345), true, Network::Testnet)
             .unwrap();
         assert_eq!(desc.to_string(), String::from("wpkh([91ef223d/84'/1'/2345']tpubDCgYuiX1p1eecECkhNc2bLSktmSDoMTj5J3v184ErUXqHTywQ7X5afv51UGfDVSaYzDWvdHhVyJ6UK8fM27EwGByWdczEERfAA9j2nzHUAj/1/*)#tj43jnd8"));
     }
