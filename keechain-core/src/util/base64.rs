49d519a341ed8cca999ae22459f62ecafb6f77ec
Yuki Kishimoto <yukikishimoto@protonmail.com>
2023-08-21 10:05:15 +0200

    core: set `base64` to v0.13

--- keechain-core/src/util/base64.rs
+++ keechain-core/src/util/base64.rs
@@ -1,19 +1,18 @@
 // Copyright (c) 2022-2023 Yuki Kishimoto
 // Distributed under the MIT software license
 
-use base64::engine::{general_purpose, Engine};
 pub use base64::DecodeError;
 
 pub fn encode<T>(input: T) -> String
 where
     T: AsRef<[u8]>,
 {
-    general_purpose::STANDARD.encode(input)
+    base64::encode(input)
 }
 
 pub fn decode<T>(input: T) -> Result<Vec<u8>, DecodeError>
 where
     T: AsRef<[u8]>,
 {
-    general_purpose::STANDARD.decode(input)
+    base64::decode(input)
 }
