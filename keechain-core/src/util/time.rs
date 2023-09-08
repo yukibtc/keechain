ff8c2dceb9a5376683743847df6d32973a60968f
Yuki Kishimoto <yukikishimoto@protonmail.com>
2023-09-08 09:47:38 +0200

    core: fix `timestamp` func on WASM

--- keechain-core/src/util/time.rs
+++ keechain-core/src/util/time.rs
@@ -1,18 +1,25 @@
 // Copyright (c) 2021-2022 Yuki Kishimoto
 // Distributed under the MIT software license
 
+#[cfg(not(target_arch = "wasm32"))]
 use std::time::{SystemTime, UNIX_EPOCH};
+
+#[cfg(target_arch = "wasm32")]
+pub use instant::SystemTime;
+
+#[cfg(target_arch = "wasm32")]
+const UNIX_EPOCH: SystemTime = SystemTime::UNIX_EPOCH;
 
 pub fn timestamp() -> u64 {
     SystemTime::now()
         .duration_since(UNIX_EPOCH)
-        .expect("Invalid system time")
+        .unwrap_or_default()
         .as_secs()
 }
 
 pub fn timestamp_nanos() -> u128 {
     SystemTime::now()
         .duration_since(UNIX_EPOCH)
-        .expect("Invalid system time")
+        .unwrap_or_default()
         .as_nanos()
 }
