293896d5c8d6a75af69d338f848f49415d4d9a3a
Yuki Kishimoto <yukikishimoto@protonmail.com>
2023-10-03 10:45:08 +0200

    core: improve `hex` module

--- keechain-core/src/util/hex.rs
+++ keechain-core/src/util/hex.rs
@@ -1,12 +1,20 @@
 // Copyright (c) 2022-2023 Yuki Kishimoto
 // Distributed under the MIT software license
 
+//! Hex
+
 use core::fmt;
 
+/// Hex error
 #[derive(Debug, PartialEq, Eq)]
 pub enum Error {
     /// An invalid character was found
-    InvalidHexCharacter { c: char, index: usize },
+    InvalidHexCharacter {
+        /// Char
+        c: char,
+        /// Char index
+        index: usize,
+    },
     /// A hex string's length needs to be even, as two digits correspond to
     /// one byte.
     OddLength,
@@ -18,23 +26,34 @@ impl fmt::Display for Error {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
         match self {
             Self::InvalidHexCharacter { c, index } => {
-                write!(f, "Invalid character {c} at position {index}")
+                write!(f, "Invalid character {} at position {}", c, index)
             }
             Self::OddLength => write!(f, "Odd number of digits"),
         }
     }
 }
 
+#[inline]
+fn from_digit(num: u8) -> char {
+    if num < 10 {
+        (b'0' + num) as char
+    } else {
+        (b'a' + num - 10) as char
+    }
+}
+
+/// Hex encode
 pub fn encode<T>(data: T) -> String
 where
     T: AsRef<[u8]>,
 {
     let bytes: &[u8] = data.as_ref();
     let mut hex: String = String::with_capacity(2 * bytes.len());
-    bytes
-        .iter()
-        .for_each(|b| hex.push_str(format!("{b:02X}").as_str()));
-    hex.to_lowercase()
+    for byte in bytes.iter() {
+        hex.push(from_digit(byte >> 4));
+        hex.push(from_digit(byte & 0xF));
+    }
+    hex
 }
 
 const fn val(c: u8, idx: usize) -> Result<u8, Error> {
@@ -49,18 +68,27 @@ const fn val(c: u8, idx: usize) -> Result<u8, Error> {
     }
 }
 
+/// Hex decode
 pub fn decode<T>(hex: T) -> Result<Vec<u8>, Error>
 where
     T: AsRef<[u8]>,
 {
     let hex = hex.as_ref();
-    if hex.len() % 2 != 0 {
+    let len = hex.len();
+
+    if len % 2 != 0 {
         return Err(Error::OddLength);
     }
-    hex.chunks(2)
-        .enumerate()
-        .map(|(i, pair)| Ok(val(pair[0], 2 * i)? << 4 | val(pair[1], 2 * i + 1)?))
-        .collect()
+
+    let mut bytes: Vec<u8> = Vec::with_capacity(len / 2);
+
+    for i in (0..len).step_by(2) {
+        let high = val(hex[i], i)?;
+        let low = val(hex[i + 1], i + 1)?;
+        bytes.push(high << 4 | low);
+    }
+
+    Ok(bytes)
 }
 
 #[cfg(test)]
